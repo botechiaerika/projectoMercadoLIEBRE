ae5ac5bba6ce337a68eeef58ba219c36e33741cb
Erika Botechia <mendoncabotechiaerikaluisa@gmail.com>
2022-01-25 09:20:38 -0300

    rutas

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const path = requiere('path')
+app publicFolderPath = path.resolve(__dirname, './public')
+app.use(express.static(publicFolderPath))
 
-app.get('/', (req, res) => {
-    console.log('Cargar INDEX')
+app.get('/home', (req, res) => {
+    console.log(__dirname); // mostra variable ubicacion archivo html en pc
+    let htmlHome = path.resolve(__dirname, './index.hml')
+    res.sendFile(htmlHome)
 })
+
 
 app.listen(3030, () => {
     console.log("LISTEN WEB SERVER ON PORT 3030")
