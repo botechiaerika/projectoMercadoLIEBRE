4469ed5f9b52ea116538bef562d92a2772dcac69
BotechiaErika <mendoncabotechiaerikaluisa@gmail.com>
2022-01-29 17:22:08 -0300

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const path = require('path')
+<<<<<<< Updated upstream
 const resolve = path.express(__dirname
         const resolve = path.resolve(__dirname + './public')
 
@@ -11,3 +12,15 @@ const resolve = path.express(__dirname
             res.sendFile(path.resolve(__dirname, ''
                 views / home.html))
         })
+=======
+
+const resolve = path.resolve(__dirname + './public')
+
+
+app.listen(3030, () => {
+    console.log('LISTEN WEB SERVER ON PORT 3030')
+}) app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, ''
+        views / home.html))
+})
+>>>>>>> Stashed changes
