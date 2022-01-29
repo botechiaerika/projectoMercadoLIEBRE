b53fa63c7f8bc3c384a59b406ca047a8aff22430
BotechiaErika <mendoncabotechiaerikaluisa@gmail.com>
2022-01-29 15:39:24 -0300

    home 3030

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 app.use(express.static('public'));
 
 app.listen(3030, () => {
-  console.log('LEVANTE y ESCUCHE WS en PORT 3030');
+    console.log('LEVANTE y ESCUCHE WS en PORT 3030');
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/home.html');
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html');
 });
