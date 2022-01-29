4712ad0d9cacaa52feebae51dcabb6815ec5fcb6
BotechiaErika <mendoncabotechiaerikaluisa@gmail.com>
2022-01-29 15:36:28 -0300

    ws 3030 port

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const app = express();
-app.use(express.static('/public'));
+app.use(express.static('public'));
 
-app.listen(5000, () => {
-  console.log('Levante WS EXPRESS on 5000');
+app.listen(3030, () => {
+  console.log('LEVANTE y ESCUCHE WS en PORT 3030');
 });
 
-let client = path('/__dirname');
-console.log(client);
-const pathPublic = path.join(client, './public');
-const resolve = path.join(pathPublic, './path');
-
-app.get('/home', (req, res) => {
-  res.sendFile(path.resolve('./views/home.html'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/home.html');
 });
-
-app.use(express.static(pathPublic));
