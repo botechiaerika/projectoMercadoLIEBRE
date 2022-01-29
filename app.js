6fd021876e75823533598527dceabb42b4c424bd
BotechiaErika <mendoncabotechiaerikaluisa@gmail.com>
2022-01-29 13:28:38 -0300

    5070 app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express()
 const path = require('path')
-const resolve = path.express(__dirname
-        const resolve = path.resolve(__dirname + './public')
+const resolve = path.resolve(__dirname + './public')
 
 
-        app.listen(3030, () => {
-            console.log('LISTEN WEB SERVER ON PORT 3030')
-        }) app.get('/', (req, res) => {
-            res.sendFile(path.resolve(__dirname, ''
-                views / home.html))
-        })
+app.listen(3030, () =>
+    console.log('LISTEN WEB SERVER ON PORT 3030')
+)
+
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/home.html'))
+})
