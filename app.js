10375b52b99c6f07da8d31f4be23fc28a527a0da
BotechiaErika <mendoncabotechiaerikaluisa@gmail.com>
2022-01-29 01:29:07 -0300

    ws 3039

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 const express = require('express');
 const app = express()
 const path = require('path')
+const resolve = path.express(__dirname
+        const resolve = path.resolve(__dirname + './public')
 
 
-app.listen(3030, () => {
-    console.log('LISTEN WEB SERVER ON PORT 3030')
-})
-
-app.get('./home', (req, res) => {
-        let homeHTML = __dirname {
-            res.send('./views/home.HTML')
+        app.listen(3030, () => {
+            console.log('LISTEN WEB SERVER ON PORT 3030')
+        }) app.get('/', (req, res) => {
+            res.sendFile(path.resolve(__dirname, ''
+                views / home.html))
         })
-}
-app.get('./*', (req, res) => {
-    app.use(path.express(path.join(__dirname, './public')))
-})
-const nodemon = require('nodemon')
-    //Metodo sendFile
-    // Envia ARCHIVOS a WS
-    //   RESPONDE A PETICION DE ESTRUCTURA ORGANIZADA Y COMPLEXA
-    //  AUMENTO CADENA DE TEXTO UBICACION AL NAVEGADOR
-    // PATH CON RUTAS ABSOLUTAS + COMODAS PARA EXPRESS
