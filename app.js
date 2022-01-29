96c2ec41ffae6b9519579912fd2476ee44a76513
BotechiaErika <mendoncabotechiaerikaluisa@gmail.com>
2022-01-29 14:13:12 -0300

    install npm --save express

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const app = express()
-const path = require('path')
-const resolve = path.resolve(__dirname + './public')
+const express = require('express')
+cons app = express()
+
+app.listen(3030, () => {
+	console.log(‘scream en 3000 port can you listen’)})
+/*
+// command c frena web server
+
+const. path = requiere(‘path’)
+// libreria path unifica rutas en distintos sistemas operativos para express llegar a estas con seguridad  
+// metodo resolve —> resolve ruta en todos dispositivos
+//dirname referencia a ubicacion en que encontro desde raiz hacia public
 
 
-app.listen(3030, () =>
-    console.log('LISTEN WEB SERVER ON PORT 3030')
-)
+const publicPath = path.join(__dirname, ‘./public’); 
+app.use( express.static(path.resolve(publicPath))
 
-
-app.get('/home', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'views/home.html'))
-})
