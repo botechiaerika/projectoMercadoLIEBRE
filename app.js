c9279e51ea5a238b94e0ed83ef49c09a37fb7cf9
BotechiaErika <mendoncabotechiaerikaluisa@gmail.com>
2022-01-29 16:10:04 -0300

    Revert "Revert "Merge remote-tracking branch 'origin/sprint-01'""
    This reverts commit c10f45090556e8fafa3d5a6ae0b5575656cc0144.

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const app = express()
-const path = require('path')
-const resolve = path.express(__dirname
-        const resolve = path.resolve(__dirname + './public')
+const app = express();
+app.use(express.static('public'));
 
+app.listen(3030, () => {
+    console.log('LEVANTE y ESCUCHE WS en PORT 3030');
+});
 
-        app.listen(3030, () => {
-            console.log('LISTEN WEB SERVER ON PORT 3030')
-        }) app.get('/', (req, res) => {
-            res.sendFile(path.resolve(__dirname, ''
-                views / home.html))
-        })
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html');
+});
