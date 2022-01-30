ae70b28758a0f48b95856c479fb42f9e51d57826
Botechia Erika <mendoncabotechiaerikaluisa@gmail.com>
2022-01-30 18:44:31 -0300

    serve-favicon

--- app.js
+++ app.js
@@ -2,21 +2,14 @@ const express = require('express');
 const port = 3030
 console.log(__dirname)
 const app = express()
-
+var path = require('path')
+var favocon = require('serve-favicon')
 const resolve = require ('resolve')
 
-const static =app.use(express.static('/public'))
 
-app.get('/', (req, res) => {
-	res.send('indez'
-
-    var express = require('express')
-var favicon = require('serve-favicon')
-var path = require('path')
-
-var app = express()
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 // Add your routes here, etc.
-
-app.listen(3000)
+app.listen(port , (req, res) => {
+    console.log("ws rodando P3030")
+})
