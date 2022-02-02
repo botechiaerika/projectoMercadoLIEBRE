0df2c962b2f699bb8cdacfcc0ea0b8e3c80957f8
botechiaerika <mendoncabotechiaerikaluisa@gmail.com>
2022-02-02 02:45:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -24,3 +24,30 @@ app.listen(3030, () => {
         views / home.html))
 })
 >>>>>>> Stashed changes
+=======
+var path = require('path')
+var favicon = require('serve-favicon')
+const resolve = require ('resolve')
+
+
+app.use(resolve(favicon(path.join(__dirname, 'public', 'favicon.ico'))))
+    app.get('/' ,(req , res ) => {
+    res.sendFile(favicon + 'register.html')
+    });
+    pp.get('/login', (req , res ) => {
+   res.sendFile(favicon + 'login.html')
+    });
+app.get('/registro' ,(req , res ) => {
+    res.sendFile(favicon + 'register.html')
+    });
+app.get('/home', (req , res ) => {
+    res.sendFile(favicon + 'home.html')
+    });
+app.get('/404' , (req, res) => {
+ res.sendFile(favicon + 'error404.html')
+ })
+
+// Add your routes here, etc.
+app.listen(port , (req, res) => {
+    console.log("ws rodando P3030")})
+>>>>>>> f0102c85b65cf6965f5a725e3450abe47c12094e
