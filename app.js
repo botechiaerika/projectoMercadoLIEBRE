87ff9d522e3f692d2d1aa5e47261e77f89b37ebe
botechiaerika <mendoncabotechiaerikaluisa@gmail.com>
2022-02-02 02:42:45 -0300

    00

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
+const express = require('express')
 
-app.listen(3030, () => {
-    console.log('LEVANTE y ESCUCHE WS en PORT 3030');
-});
+const path = require('path')
 
-app.get('/home', (req, res) => {
-    res.sendFile(__dirname + '/views/home.html');
-});
+const port = 3065
+
+const app = express() 
+app.use(express.static(resolve(__dirname + '/public/')));
+
+ app.listen(port , () => { console.log(`Listen WS NODE EXPRESS on port ${port} by eri`)
+})
+    app.get('/' , (req , res) =>{ res.send("Bien-Venid@s a mercado LIEBRE")
+    res.sendFile(__dirname + './views/index.html')
+    });
+app.get('/login', (req , res ) => {
+   res.sendFile(__dirname + './views/login.html')
+    });
+app.get('/registro' ,(req , res ) => {
+    res.sendFile(__dirname + './views/register.html')
+    });
+app.get('/home', (req , res ) => {
+    res.sendFile('./public' + '/views/home.html')
+    });
+app.get('/404' , (req, res) => {
+ res.sendFile(__dirname + './views/error404.html')
+ })
